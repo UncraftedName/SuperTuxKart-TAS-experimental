953b9742d51986880f50fb095bde542f8283417e
Benjamin Princen <princen.benjamin@gmail.com>
2022-02-07 21:05:41 -0800

    finished most of the functionality, must test

--- Parser/parser.py
+++ Parser/parser.py
@@ -10,38 +10,76 @@
 # ==================================
 
 import pathlib
+import json
 import re
 
 test_path = "./test/tasfile"
 
 def processTASFields(fields):
-    """process each field and extract information
+    """process each field and extract information returning
+    it in dictionary form
 
 
     Keyword arguments:
     fields -- list containing all fields
+
+    Return:
+    Dictionary -- dictionary containing info on framebulk
     """
+    acc     = fields[0]
+    misc    = fields[1]
+    ang     = fields[2]
+    ticks   = fields[3]
+
+    # process acceleration fields
+    accelerate = True if acc[0] == 'a' else False
+    decelerate = True if acc[1] == 'b' else False
+
+    # process mischelleneous fields
+    fire = True if misc[0] == 'f' else False
+    nitro = True if misc[1] == 'n' else False
+    skid = True if misc[2] == 's' else False
+
+    # process turning angle
+    angleMag = float(ang)
+
+    # process ticks field
+    numTicks = int(ticks)
+
+    return {
+        "accelerate"    : accelerate,
+        "decelerate"    : decelerate,
+        "fire"          : fire,
+        "nitro"         : nitro,
+        "skid"          : skid,
+        "angleMag"      : angleMag,
+        "numTicks"      : numTicks
+    }
 
 def processTASLines(data):
-    """process lines from TAS script
+    """process lines from TAS script and return information
+    in dictionary format
 
 
     Keyword arguments:
     data -- list containing lines from TAS script
-    """
 
+    Return:
+    Dictionary -- dictionary containing all data in parsed TAS script
+    """
+    tasData = {}
     print(data)
 
     # search for map
     results = re.findall("map <([A-z ]+)>", data[0])
     if not results: # check if list is empty
         print("Warning: Map not found.")
-
+    tasData['map'] = results[0]
     # begin parsing framebulks
     if not "frames" in data[1]:
         print("Error: Did not find start of framebulks. Exiting...")
         exit(-1)
-
+    tasData['frame_bulks'] = []
     # syntax for framebulks:
     # --|---|-|0|
     # - indicate a field
@@ -54,11 +92,9 @@ def processTASLines(data):
         if len(fields) != 4:
             print("Warning: Error parsing framebulk (line " + str(line_num) + "). Skipping...")
             continue
-        acc =   fields[0]
-        misch = fields[1]
-        ang =   fields[2]
-        ticks = fields[3]
-        processTASFields(fields)
+        tasData['frame_bulks'].append(processTASFields(fields))
+
+    return tasData
 
 def removeComments(string): # literally hear just to remove comments :P
     return string[:string.find("//")]
@@ -81,11 +117,10 @@ def parseTAS(tasFile):
 
     data = [removeComments(x) for x in file.readlines() if (x != "\n")] # removes empty lines from list
 
-
-
-    processTASLines(data)
-
     file.close()
+
+    return processTASLines(data)
+
 
 
 
@@ -96,7 +131,11 @@ def main():
     """
     """
     global test_path
-    parseTAS(test_path)
+    tasInfo = parseTAS(test_path)
+
+    with open('tasData.json', 'w') as file:
+        json.dump(tasInfo, file)
+    
 
 if __name__ == "__main__":
     main()
